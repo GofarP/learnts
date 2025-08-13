05f897ad6275b9867917553b6511fb0c8ee13020
GofarP <gofarperdana83@gmail.com>
2025-08-13 16:37:59 +0700

    Object with optional parameter

--- Object.ts
+++ Object.ts
@@ -1,11 +1,28 @@
-const person:{id:string, name:string}={
-    id:"1",
-    name:"Gofar"
+type Person = {
+    id: string;
+    name: string;
+    hobies?: string
+}
+
+const person: Person = {
+    id: "1",
+    name: "Gofar",
+    hobies: "rebahan"
 };
 
-console.info(person);
+function printPerson(p: Person) {
+    if (p.id === "1") {
+        console.info(p)
+    } else {
+        const {hobies, ...rest}=p;
+        console.info(rest);
+    }
+}
 
-person.id="2";
-person.name="Putra";
+printPerson(person);
 
-console.info(person);
+
+person.id="2"
+person.name="Putra"
+
+printPerson(person)
